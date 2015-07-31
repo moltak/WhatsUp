1c015c0185dd9af816e9e6589f1f09c2425dd4e9
moltak <moltak@gmail.com>
2015-07-31 16:04:10 -0700

    I has putted toolbar on MainActivity.

--- app/src/main/java/org/highway/whatsup/ui/activity/MainActivity.java
+++ app/src/main/java/org/highway/whatsup/ui/activity/MainActivity.java
@@ -1,15 +1,59 @@
 package org.highway.whatsup.ui.activity;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
 
 import org.highway.whatsup.R;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.mainToolBar) Toolbar mainToolBar;
+    @Bind(R.id.mainDrawerNavigationView) NavigationView mainDrawerNavigationView;
+    @Bind(R.id.mainDrawerLayout) DrawerLayout mainDrawerLayout;
+
+    private ActionBarDrawerToggle drawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        setSupportActionBar(mainToolBar);
+        drawerToggle = new ActionBarDrawerToggle(this, mainDrawerLayout,
+                mainToolBar, R.string.app_name, R.string.app_name);
+        drawerToggle.setDrawerIndicatorEnabled(true);
+        mainDrawerLayout.setDrawerListener(drawerToggle);
+        mainDrawerNavigationView.setNavigationItemSelectedListener(new MainNavitationItemSelectedListener());
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    private class MainNavitationItemSelectedListener implements NavigationView.OnNavigationItemSelectedListener {
+        @Override
+        public boolean onNavigationItemSelected(MenuItem menuItem) {
+            return false;
+        }
     }
 }
