4230bb12c9d5d3901e216f5d868ed41d3004d22a
moltak <moltak@gmail.com>
2015-08-24 22:45:11 -0700

    fake objects 추가. 기능 구현은 아직 안되어있음. 기능 구현 후 이름 변경

---
+++ data/src/main/java/org/highway/whatsup/data/mobileinfo/fakeUuidGenerator.java
@@ -0,0 +1,13 @@
+package org.highway.whatsup.data.mobileinfo;
+
+import android.content.Context;
+
+/**
+ * Created by engeng on 8/24/15.
+ * todo
+ */
+public class fakeUuidGenerator {
+    public static String gen(Context context) {
+        return "uuid";
+    }
+}
