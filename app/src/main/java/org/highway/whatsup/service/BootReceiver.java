19dccf00ad934950e33d9253892b0cf6900b182c
moltak <moltak@gmail.com>
2015-08-26 21:39:26 -0700

    Add BootReciever. It is associated with the auto start service. It can be aware of the mobile startup.

---
+++ app/src/main/java/org/highway/whatsup/service/BootReceiver.java
@@ -0,0 +1,18 @@
+package org.highway.whatsup.service;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by engeng on 8/26/15.
+ */
+public class BootReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
+            Intent i = new Intent(context, LocationUpdateService.class);
+            context.startService(i);
+        }
+    }
+}
