4ce7700c693604e80d1dcf6e074c28027cdf99b7
moltak <moltak@gmail.com>
2015-08-28 21:57:38 -0700

    Complete AlertActivity

--- app/src/main/java/org/highway/whatsup/service/MainService.java
+++ app/src/main/java/org/highway/whatsup/service/MainService.java
@@ -4,7 +4,6 @@ import android.app.Service;
 import android.content.Intent;
 import android.location.Location;
 import android.os.IBinder;
-import android.util.Log;
 
 import com.google.android.gms.location.LocationRequest;
 
